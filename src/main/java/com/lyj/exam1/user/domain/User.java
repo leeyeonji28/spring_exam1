feca9e97146e68c0a181cef6a128e2c23365df01
leeyeonji28 <yeon102800@gmail.com>
2022-06-30 01:37:08 +0900

    user 엔티티 추가

---
+++ src/main/java/com/lyj/exam1/user/domain/User.java
@@ -0,0 +1,14 @@
+package com.lyj.exam1.user.domain;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    public long id;
+    public LocalDateTime regDate;
+    public LocalDateTime updateDate;
+    public String email;
+    public String password;
+    public String name;
+}

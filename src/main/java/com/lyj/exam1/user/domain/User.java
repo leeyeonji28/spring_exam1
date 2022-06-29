ac0ad0b1f57a602cd7daacc0984622acf9993df9
leeyeonji28 <yeon102800@gmail.com>
2022-06-30 01:56:41 +0900

    lombok 적용

--- src/main/java/com/lyj/exam1/user/domain/User.java
+++ src/main/java/com/lyj/exam1/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.lyj.exam1.user.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
@@ -8,22 +9,15 @@ import javax.persistence.GenerationType;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class User {
     @javax.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long id;
-    public LocalDateTime regDate;
-    public LocalDateTime updateDate;
-    public String email;
-    public String password;
-    public String name;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+    private long id;
+    private LocalDateTime regDate;
+    private LocalDateTime updateDate;
+    private String email;
+    private String password;
+    private String name;
 }
